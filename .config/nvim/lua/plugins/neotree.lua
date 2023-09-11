39b06ca3154040a223f3175e40f26a4cd4f439e9
Francisco Millone <franba2010@gmail.com>
2023-09-10 23:57:30 -0300

    updated neovim config

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -1,6 +1,13 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
-    -- close_if_last_window = true,
+    filesystem = {
+      filtered_items = {
+        always_show = {
+          ".gitignore",
+          ".config",
+        },
+      },
+    },
   },
 }
