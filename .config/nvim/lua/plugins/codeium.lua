cbba90f6ba82c9b858993699209199551cda2704
Francisco Millone <franba2010@gmail.com>
2023-09-27 20:49:49 -0300

    added codeium config

---
+++ .config/nvim/lua/plugins/codeium.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "Exafunction/codeium.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "hrsh7th/nvim-cmp",
+    },
+    config = function()
+      require("codeium").setup({})
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, {
+        { name = "codeium", keyword_length = 1, max_item_count = 5, group_index = 2 },
+      }))
+    end,
+  },
+}
