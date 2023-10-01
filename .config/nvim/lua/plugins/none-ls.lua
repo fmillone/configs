65cf86c2014f981712be7fbe5b3d88553bcb4131
Francisco Millone <franba2010@gmail.com>
2023-09-30 23:17:09 -0300

    minor cleanup

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -4,7 +4,6 @@ return {
   opts = function(_, opts)
     if type(opts.sources) == "table" then
       local null_ls = require("null-ls")
-      vim.print("Using none-ls.nvim")
       vim.list_extend(opts.sources, {
         null_ls.builtins.code_actions.gitsigns,
         null_ls.builtins.code_actions.refactoring,
