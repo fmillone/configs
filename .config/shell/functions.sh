84f338029f2f84d20a4e01e78975cdb811c78aba
Francisco Millone <fmillone@users.noreply.github.com>
2023-10-05 19:06:20 -0300

    added aws function helpers

--- .config/shell/functions.sh
+++ .config/shell/functions.sh
@@ -15,3 +15,25 @@ addToPathFront() {
 gro() {
 	git rebase origin/$1
 }
+
+setAwsEnv() {
+	if [ -z "$1" ]; then
+		echo "No argument passed"
+		return 1
+	fi
+	# check if the profile exists
+	if aws configure list-profiles | grep -q "^$1$"; then
+		export AWS_REGION=$(aws configure get region --profile $1)
+		export AWS_PROFILE=$1
+		echo "Now using AWS_PROFILE=$AWS_PROFILE and AWS_REGION=$AWS_REGION"
+	else
+		echo "Profile $1 does not exist"
+		return 1
+	fi
+}
+
+unsetAwsEnv() {
+	unset AWS_REGION
+	unset AWS_PROFILE
+	echo "AWS_PROFILE and AWS_REGION unset"
+}
