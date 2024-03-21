381a8e1e5dd845256290cb402cf9968de2c25b5a
Francisco Millone <franba2010@gmail.com>
2024-03-21 00:29:55 -0300

    nvim telescope git status. updated tmux theme

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -5,5 +5,10 @@ return {
     opts = function(_, opts)
       opts.defaults.layout_strategy = "vertical"
     end,
+    config = function(_, opts)
+      require("telescope").setup(opts)
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>sgs", builtin.git_status, { desc = "[S]earch in [G]it [S]tatus" })
+    end,
   },
 }
