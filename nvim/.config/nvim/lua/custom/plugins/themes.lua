a35a69147b0ceeaa230d0f79aaaa32652b8a7a9f
Francisco Millone <franba2010@gmail.com>
2024-05-29 23:16:52 -0300

    select theme based on os

--- nvim/.config/nvim/lua/custom/plugins/themes.lua
+++ nvim/.config/nvim/lua/custom/plugins/themes.lua
@@ -1,17 +1,22 @@
+local function set_colorscheme()
+  if vim.fn.has 'macunix' then
+    vim.cmd.colorscheme 'tokyonight-night'
+  else
+    vim.cmd.colorscheme 'onedark'
+  end
+end
+
 return {
   {
     'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      vim.cmd.colorscheme 'tokyonight-night'
-
-      -- You can configure highlights by doing something like:
-      -- vim.cmd.hi 'Comment gui=none'
-    end,
+    init = set_colorscheme,
   },
   { 'EdenEast/nightfox.nvim' },
   {
     'navarasu/onedark.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    init = set_colorscheme,
     opts = {
       style = 'darker',
     },
