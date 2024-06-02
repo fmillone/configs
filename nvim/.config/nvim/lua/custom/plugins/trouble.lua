c7af1d0838e7cff17bc77260e70d10f8187af304
Francisco Millone <franba2010@gmail.com>
2024-06-02 20:57:47 -0300

    nvim oil show hidden. update trouble

--- nvim/.config/nvim/lua/custom/plugins/trouble.lua
+++ nvim/.config/nvim/lua/custom/plugins/trouble.lua
@@ -2,13 +2,14 @@ return {
   {
     'folke/trouble.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-    branch = 'dev', -- IMPORTANT!
+    -- branch = 'dev', -- IMPORTANT!
     priority = 900,
     opts = {
       -- your configuration comes here
       -- or leave it empty to use the default settings
       -- refer to the configuration section below
     },
+    cmd = 'Trouble',
     keys = {
       {
         '<leader>xx',
