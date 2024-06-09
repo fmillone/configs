3845b55834f9b428f91ac4e2f7fb30c06116935e
Francisco Millone <franba2010@gmail.com>
2024-06-09 12:51:38 -0300

    added oil path to lualine

--- nvim/.config/nvim/lua/custom/plugins/lualine.lua
+++ nvim/.config/nvim/lua/custom/plugins/lualine.lua
@@ -15,6 +15,15 @@ return {
         filter = { range = true },
         format = '{kind_icon}{symbol.name:Normal}',
       }
+      local oil = require 'oil'
+      local oil_status = {
+        has = function()
+          return vim.bo.filetype == 'oil'
+        end,
+        get = function()
+          return oil.get_current_dir()
+        end,
+      }
       local noice_status = require('noice').api.status.mode
       return {
         options = {
@@ -40,6 +49,7 @@ return {
         tabline = {},
         winbar = {
           lualine_a = { { symbols.get, cond = symbols.has } },
+          lualine_b = { { oil_status.get, cond = oil_status.has } },
           lualine_y = { 'encoding', 'fileformat', 'filetype' },
         },
       }
