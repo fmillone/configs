b4b30b846edd73c948b025ed8a6ffd577a7535db
Francisco Millone <franba2010@gmail.com>
2024-06-18 14:03:16 +0000

    updated nvim checkhealth

--- nvim/.config/nvim/lua/kickstart/health.lua
+++ nvim/.config/nvim/lua/kickstart/health.lua
@@ -21,7 +21,7 @@ end
 
 local check_external_reqs = function()
   -- Basic utils: `git`, `make`, `unzip`
-  for _, exe in ipairs { 'git', 'make', 'unzip', 'rg' } do
+  for _, exe in ipairs { 'git', 'make', 'unzip', 'rg', 'fzf', 'fd' } do
     local is_executable = vim.fn.executable(exe) == 1
     if is_executable then
       vim.health.ok(string.format("Found executable: '%s'", exe))
